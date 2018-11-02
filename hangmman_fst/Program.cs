5586017f01e05de88079954c6c8b15f463a06bb5
Zoltan Lengyel <zoltanlengyel77@gmail.com>
2018-11-02 08:47:56 +0100

    Add project files.

---
+++ hangmman_fst/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hangmman_fst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
