895c6c5b84130cf8bf32cc0a375b530665130bcf
Zoltan Lengyel <zoltanlengyel77@gmail.com>
2018-11-02 09:26:22 +0100

    Wordlist stage one

--- hangmman_fst/Program.cs
+++ hangmman_fst/Program.cs
@@ -10,7 +10,11 @@ namespace hangmman_fst
     {
         static void Main(string[] args)
         {
+            words list = new words;
             draw_man hMan = new draw_man("", 0);
+
+            Console.WriteLine(list);
+
             Console.ReadKey();
         }
     }
---
+++ hangmman_fst/words.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hangmman_fst
+{
+    class Words
+    {
+        public Words() { }
+
+        static void readFile()
+        {
+            using (StreamReader wordFile = new StreamReader("wordList.txt"))
+            {
+                while (!wordFile.EndOfStream)
+                {
+                    string line = wordFile.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        
+    }
+}
